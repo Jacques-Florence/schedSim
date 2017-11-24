49b37b97875f29ac975f83ae626a9bc639b4404e
Jacques Florence <jacques.p.florence@gmail.com>
2017-11-24 08:08:23 -0800

    small cleanup

--- src/buildScript.sh
+++ src/buildScript.sh
@@ -1,11 +1,6 @@
 mkdir -p ../build
 cd ../build
-#../../cmake-3.5.2/bin/cmake ../src -DCMAKE_CXX_COMPILER=g++
-#/usr/local/bin/cmake ../src -DCMAKE_CXX_COMPILER=g++
-#module load cmake/3.5.2
 cmake ../src -DCMAKE_CXX_COMPILER=g++
-
-
 
 if make ; then
 
