94c88281c3fa830af2affbcc0c3981e69e72dd05
Jacques Florence <jacques.p.florence@gmail.com>
2017-11-24 08:39:03 -0800

    added a configuration.conf to the docs folder

--- docsrc/docmain.cpp
+++ docsrc/docmain.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <scheduler/freqConstants.h>
 #include <scheduler/schedulingSimulator.h>
 #include <scheduler/schedulerConfiguration.h>
 
@@ -8,12 +9,11 @@ int main()
 	Scheduler::SchedulingSimulator simulator;
 	simulator.seedRandomGenerator(123);
 	
-	simulator.createRealTimeTask(0.0, 15.0, 15.0, 7.0);
-	simulator.createInteractiveProcess(1.0);
+	simulator.createRealTimeTask(0.0, 15.0, 15.0*MINFREQ,  7.0);
+	simulator.createRealTimeTask(0.0, 20.0, 15.0*MINFREQ, 20.0);
 
 	simulator.turnOnProcessor(0.0);
-	simulator.setSchedulerTimeout(1.0, 2.0);
-	simulator.endSimulation(50);
+	simulator.endSimulation(600);
 
 	simulator.startScheduler();
 	return 0;
